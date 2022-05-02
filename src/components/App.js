b05d3839e323bbbbd50be93f203ef4f42af32cc4
Akshay Harith <ak.prodigy24@gmail.com>
2022-05-02 13:27:00 +0530

    Task: connect SongDetail component to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import actions from '../actions';
 
 import SongList from './SongList';
+import SongDetail from './SongDetail';
 
 const App = () => {
 	return (
 		<div className="App">
 			<h1>App</h1>
 			<SongList />
+			<SongDetail />
 		</div>
 	)
 }
