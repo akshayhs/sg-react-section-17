b44b494362d0b7fe3703d446d09af5c2d6acb554
Akshay Harith <ak.prodigy24@gmail.com>
2022-05-02 12:25:59 +0530

    Add: jsx for List data in SongList

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -2,8 +2,21 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class SongList extends Component {
+	renderList = () => {
+		/* Used to return the shadow array of JSX elements */
+		return this.props.songs.map((song, i) => {
+			/* Used to return JSX for the component */
+			return (
+				<div className="list-item" key={i}>
+					<h4>{song.title}</h4>
+					<button className="btn btn-primary">Select</button>					
+				</div>
+			)
+		});
+	}
+
 	render() {
-		return <div>Song list</div>;
+		return <div>{this.renderList()}</div>;
 	}
 }
 
